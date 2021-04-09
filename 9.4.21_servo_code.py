e7a14b8526065642f1ab1b20e290eec843a6e28b
Avinash10000 <74100481+Avinash10000@users.noreply.github.com>
2021-04-09 20:08:26 +0100

    Update 9.4.21_servo_code.py

--- 9.4.21_servo_code.py
+++ 9.4.21_servo_code.py
@@ -5,6 +5,19 @@
 # Import libraries
 import RPi.GPIO as GPIO
 import time
+
+  
+# Set GPIO numbering mode
+GPIO.setmode(GPIO.BOARD)
+
+# Set pin 11 as an output, and define as servo1 as PWM pin
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
+
+# Start PWM running, with value of 0 (pulse off)
+servo1.start(0)
+
+
 def cleaning_things_at_the_end():
   #Clean things up at the end
   servo1.stop()
@@ -20,21 +33,19 @@ def moving_motor_90_clockwise():
   time.sleep(0.5)
   servo1.ChangeDutyCycle(0)
   
+'''defining the movement (90 degrees anti-clockwise)'''
+# time statment can be anything
+# ChangeDutyCycle = 0 removes jitters by servo motor
+# adding 2 is necessary
+def moving_motor_90_anti_clockwise():
+  servo1.ChangeDutyCycle(2+(90/18))
+  time.sleep(0.5)
+  servo1.ChangeDutyCycle(0)
   
-# Set GPIO numbering mode
-GPIO.setmode(GPIO.BOARD)
+  
 
-# Set pin 11 as an output, and define as servo1 as PWM pin
-GPIO.setup(11,GPIO.OUT)
-servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
-
-# Start PWM running, with value of 0 (pulse off)
-servo1.start(0)
-
-# Loop to allow user to set servo angle. Try/finally allows exit
-# with execution of servo.stop and GPIO cleanup :)
-
-
+# moving_motor_90_clockwise():
+# moving_motor_90_anti_clockwise():
 cleaning_things_at_the_end()
 
 
